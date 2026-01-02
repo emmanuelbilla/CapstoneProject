5ccac0e1219e5c8cb1f6c94151ca94021212cc92
emmanuelbilla <immanuelbilla33@gmail.com>
2026-01-02 15:54:34 +0000

    Fix assignment serializer by explicitly setting read-only fields

--- Project_Management_API/assignments/serializers.py
+++ Project_Management_API/assignments/serializers.py
@@ -5,7 +5,8 @@ from .models import VolunteerAssignment
 # Serializer for VolunteerAssignment model
 class VolunteerAssignmentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+
     class Meta:
         model = VolunteerAssignment
-        fields = '__all__'
+        fields = ['id', 'owner', 'user', 'task', 'joined_at']
+        read_only_fields = ['owner', 'joined_at']
